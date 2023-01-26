7d393a0131f49f301c620156152e272279d0aa81
lostluck <13907733+lostluck@users.noreply.github.com>
2023-01-25 16:16:17 -0800

    [local] reorganize elementmanager.go

--- local/internal/elementmanager.go
+++ local/internal/elementmanager.go
@@ -96,92 +96,6 @@ func (em *elementManager) addStage(ID string, inputIDs, sides, outputIDs []strin
 	}
 }
 
-type stageState struct {
-	ID        string
-	inputID   string   // PCollection ID of the parallel input
-	outputIDs []string // PCollection IDs of outputs to update consumers.
-	sides     []string // PCollection IDs of side inputs that can block execution.
-	strat     winStrat
-
-	mu                 sync.Mutex
-	upstreamWatermarks sync.Map   // watermark set from inputPCollection's parent.
-	input              mtime.Time // input watermark for the parallel input.
-	output             mtime.Time // Output watermark for the whole stage
-
-	pending    elementHeap         // pending input elements for this stage that are to be processesd
-	inprogress map[string]elements // inprogress elements by active bundles, keyed by bundle
-}
-
-// makeStageState produces an initialized stage stage.
-func makeStageState(ID string, inputIDs, sides, outputIDs []string) *stageState {
-	ss := &stageState{
-		ID:        ID,
-		outputIDs: outputIDs,
-		sides:     sides,
-		strat:     defaultStrat{},
-
-		input:  mtime.MinTimestamp,
-		output: mtime.MinTimestamp,
-	}
-
-	// Initialize the upstream watermarks to minTime.
-	for _, pcol := range inputIDs {
-		ss.upstreamWatermarks.Store(pcol, mtime.MinTimestamp)
-	}
-	if len(inputIDs) == 1 {
-		ss.inputID = inputIDs[0]
-	}
-	logger.Logf("makeStageState: %v input: %v sides: %v outputs: %v", ID, ss.inputID, ss.sides, ss.outputIDs)
-	// If this is zero, then it's an impulse, and will sort itself out.
-	// TODO, add initial pendings here?
-	return ss
-}
-
-func (ss *stageState) AddPending(newPending []element) {
-	ss.mu.Lock()
-	defer ss.mu.Unlock()
-	ss.pending = append(ss.pending, newPending...)
-	heap.Init(&ss.pending)
-}
-
-// updateUpstreamWatermark is for the parent of the input pcollection
-// to call, to update downstream stages with it's current watermark.
-// This avoids downstream stages inverting lock orderings from
-// calling their parent stage to get their input pcollection's watermark.
-func (ss *stageState) updateUpstreamWatermark(pcol string, upstream mtime.Time) {
-	// A stage will only have a single upstream watermark, so
-	// we simply set this.
-	ss.upstreamWatermarks.Store(pcol, upstream)
-}
-
-// UpstreamWatermark get's the minimum value of all upstream watermarks.
-func (ss *stageState) UpstreamWatermark() (string, mtime.Time) {
-	upstream := mtime.MaxTimestamp
-	var name string
-	ss.upstreamWatermarks.Range(func(key, val any) bool {
-		if val.(mtime.Time) < upstream {
-			upstream = val.(mtime.Time)
-			name = key.(string)
-		}
-		return true
-	})
-	return name, upstream
-}
-
-// InputWatermark gets the current input watermark for the stage.
-func (ss *stageState) InputWatermark() mtime.Time {
-	ss.mu.Lock()
-	defer ss.mu.Unlock()
-	return ss.input
-}
-
-// OutputWatermark gets the current output watermark for the stage.
-func (ss *stageState) OutputWatermark() mtime.Time {
-	ss.mu.Lock()
-	defer ss.mu.Unlock()
-	return ss.output
-}
-
 type element struct {
 	window    typex.Window
 	timestamp mtime.Time
@@ -259,44 +173,6 @@ func (rb runBundle) String() string {
 	return fmt.Sprintf("{%v %v %v}", rb.stageID, rb.bundleID, rb.watermark)
 }
 
-func (ss *stageState) startBundle(rb runBundle) bool {
-	defer func() {
-		if e := recover(); e != nil {
-			panic(fmt.Sprintf("stage %v bundle %v at %v panicked\n%v", ss.ID, rb.bundleID, rb.watermark, e))
-		}
-	}()
-	ss.mu.Lock()
-	defer ss.mu.Unlock()
-	logger.Logf("startBundle: %v %v - %v pending elements at %v", ss.ID, rb.bundleID, len(ss.pending), rb.watermark)
-
-	var toProcess, notYet []element
-	for _, e := range ss.pending {
-		if e.window.MaxTimestamp() <= rb.watermark {
-			toProcess = append(toProcess, e)
-		} else {
-			notYet = append(notYet, e)
-		}
-	}
-	ss.pending = notYet
-	heap.Init(&ss.pending)
-
-	if len(toProcess) == 0 {
-		logger.Logf("startBundle: %v %v - no elements at %v", ss.ID, rb.bundleID, rb.watermark)
-		return false
-	}
-	// Is THIS is where basic splits should happen/per element processing?
-	es := elements{
-		es:           toProcess,
-		minTimestamp: toProcess[0].timestamp,
-	}
-	if ss.inprogress == nil {
-		ss.inprogress = make(map[string]elements)
-	}
-	ss.inprogress[rb.bundleID] = es
-	logger.Logf("startBundle: %v %v - %v elements at %v", ss.ID, rb.bundleID, len(es.es), rb.watermark)
-	return true
-}
-
 func (em *elementManager) Bundles(nextBundID func() string) <-chan runBundle {
 	runStageCh := make(chan runBundle)
 	ctx, cancelFn := context.WithCancel(context.TODO())
@@ -344,42 +220,9 @@ func (em *elementManager) Bundles(nextBundID func() string) <-chan runBundle {
 			for stageID := range advanced {
 				ss := em.stages[stageID]
 				// TODO move the loop contents to a method to simplify lock handling.
-				ss.mu.Lock()
-
-				// If the upstream watermark and the input watermark are the same,
-				// then we can't yet process this stage.
-				inputW := ss.input
-				upstreamPcol, upstreamW := ss.UpstreamWatermark()
-				if inputW == upstreamW {
-					logger.Logf("Bundles: stage %v has insufficient upstream watermark; requires %q %v > %v", stageID, upstreamPcol, inputW, upstreamW)
-					ss.mu.Unlock()
-					continue
-				} else if inputW > upstreamW {
-					ss.mu.Unlock()
-					logger.Fatalf("%v has invariance violation, input watermark greater than upstream: %v", stageID, inputW, upstreamW)
-				}
-
-				// We know now that inputW < upstreamW, so we should be able to process any available inputs.
-				if len(ss.sides) > 0 {
-					logger.Logf("Bundles: stage %v has side inputs %v", stageID, ss.sides)
-				}
-				ready := true
-				for _, side := range ss.sides {
-					pID := em.pcolParents[side]
-					parent := em.stages[pID]
-					// Possible lock inversion?
-					ow := parent.OutputWatermark()
-					logger.Logf("Bundles: stage %v parent[%v] side input %v watermark; requires %v >= %v", stageID, pID, side, ow, upstreamW)
-
-					if upstreamW > ow {
-						logger.Logf("Bundles: stage %v side input %v from %v insufficient watermark; requires %v >= %v", stageID, side, pID, ow, upstreamW)
-						ready = false
-					}
-				}
-
-				ss.mu.Unlock()
+				watermark, ready := ss.bundleReady(em)
 				if ready {
-					rb := runBundle{stageID: stageID, bundleID: nextBundID(), watermark: upstreamW}
+					rb := runBundle{stageID: stageID, bundleID: nextBundID(), watermark: watermark}
 					rb.bundleID = nextBundID()
 					ok := ss.startBundle(rb)
 					if !ok {
@@ -508,28 +351,6 @@ func (em *elementManager) PersistBundle(stageID string, bundID string, col2Coder
 	em.addRefreshAndClearBundle(stage.ID, bundID)
 }
 
-// minimumPendingTimestamp returns the minimum pending timestamp from all pending elements,
-// including in progress ones.
-//
-// Assumes that the pending heap is initialized if it's not empty.
-func (ss *stageState) minPendingTimestamp() mtime.Time {
-	ss.mu.Lock()
-	defer ss.mu.Unlock()
-	minPending := mtime.MaxTimestamp
-	if len(ss.pending) != 0 {
-		minPending = ss.pending[0].timestamp
-	}
-	for _, es := range ss.inprogress {
-		minPending = mtime.Min(minPending, es.minTimestamp)
-	}
-	return minPending
-}
-
-func (ss *stageState) String() string {
-	pcol, up := ss.UpstreamWatermark()
-	return fmt.Sprintf("[%v] IN: %v OUT: %v UP: %q %v", ss.ID, ss.input, ss.output, pcol, up)
-}
-
 func (em *elementManager) addRefreshes(stages set[string]) {
 	em.refreshCond.L.Lock()
 	defer em.refreshCond.L.Unlock()
@@ -572,6 +393,168 @@ func (em *elementManager) refreshWatermarks() set[string] {
 	}
 	em.watermarkRefreshes.merge(nextUpdates)
 	return refreshed
+}
+
+type set[K comparable] map[K]struct{}
+
+func (s set[K]) remove(k K) {
+	delete(s, k)
+}
+
+func (s set[K]) insert(k K) {
+	s[k] = struct{}{}
+}
+
+func (s set[K]) merge(o set[K]) {
+	for k := range o {
+		s.insert(k)
+	}
+}
+
+type stageState struct {
+	ID        string
+	inputID   string   // PCollection ID of the parallel input
+	outputIDs []string // PCollection IDs of outputs to update consumers.
+	sides     []string // PCollection IDs of side inputs that can block execution.
+	strat     winStrat
+
+	mu                 sync.Mutex
+	upstreamWatermarks sync.Map   // watermark set from inputPCollection's parent.
+	input              mtime.Time // input watermark for the parallel input.
+	output             mtime.Time // Output watermark for the whole stage
+
+	pending    elementHeap         // pending input elements for this stage that are to be processesd
+	inprogress map[string]elements // inprogress elements by active bundles, keyed by bundle
+}
+
+// makeStageState produces an initialized stage stage.
+func makeStageState(ID string, inputIDs, sides, outputIDs []string) *stageState {
+	ss := &stageState{
+		ID:        ID,
+		outputIDs: outputIDs,
+		sides:     sides,
+		strat:     defaultStrat{},
+
+		input:  mtime.MinTimestamp,
+		output: mtime.MinTimestamp,
+	}
+
+	// Initialize the upstream watermarks to minTime.
+	for _, pcol := range inputIDs {
+		ss.upstreamWatermarks.Store(pcol, mtime.MinTimestamp)
+	}
+	if len(inputIDs) == 1 {
+		ss.inputID = inputIDs[0]
+	}
+	logger.Logf("makeStageState: %v input: %v sides: %v outputs: %v", ID, ss.inputID, ss.sides, ss.outputIDs)
+	// If this is zero, then it's an impulse, and will sort itself out.
+	// TODO, add initial pendings here?
+	return ss
+}
+
+func (ss *stageState) AddPending(newPending []element) {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	ss.pending = append(ss.pending, newPending...)
+	heap.Init(&ss.pending)
+}
+
+// updateUpstreamWatermark is for the parent of the input pcollection
+// to call, to update downstream stages with it's current watermark.
+// This avoids downstream stages inverting lock orderings from
+// calling their parent stage to get their input pcollection's watermark.
+func (ss *stageState) updateUpstreamWatermark(pcol string, upstream mtime.Time) {
+	// A stage will only have a single upstream watermark, so
+	// we simply set this.
+	ss.upstreamWatermarks.Store(pcol, upstream)
+}
+
+// UpstreamWatermark get's the minimum value of all upstream watermarks.
+func (ss *stageState) UpstreamWatermark() (string, mtime.Time) {
+	upstream := mtime.MaxTimestamp
+	var name string
+	ss.upstreamWatermarks.Range(func(key, val any) bool {
+		if val.(mtime.Time) < upstream {
+			upstream = val.(mtime.Time)
+			name = key.(string)
+		}
+		return true
+	})
+	return name, upstream
+}
+
+// InputWatermark gets the current input watermark for the stage.
+func (ss *stageState) InputWatermark() mtime.Time {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	return ss.input
+}
+
+// OutputWatermark gets the current output watermark for the stage.
+func (ss *stageState) OutputWatermark() mtime.Time {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	return ss.output
+}
+
+func (ss *stageState) startBundle(rb runBundle) bool {
+	defer func() {
+		if e := recover(); e != nil {
+			panic(fmt.Sprintf("stage %v bundle %v at %v panicked\n%v", ss.ID, rb.bundleID, rb.watermark, e))
+		}
+	}()
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	logger.Logf("startBundle: %v %v - %v pending elements at %v", ss.ID, rb.bundleID, len(ss.pending), rb.watermark)
+
+	var toProcess, notYet []element
+	for _, e := range ss.pending {
+		if e.window.MaxTimestamp() <= rb.watermark {
+			toProcess = append(toProcess, e)
+		} else {
+			notYet = append(notYet, e)
+		}
+	}
+	ss.pending = notYet
+	heap.Init(&ss.pending)
+
+	if len(toProcess) == 0 {
+		logger.Logf("startBundle: %v %v - no elements at %v", ss.ID, rb.bundleID, rb.watermark)
+		return false
+	}
+	// Is THIS is where basic splits should happen/per element processing?
+	es := elements{
+		es:           toProcess,
+		minTimestamp: toProcess[0].timestamp,
+	}
+	if ss.inprogress == nil {
+		ss.inprogress = make(map[string]elements)
+	}
+	ss.inprogress[rb.bundleID] = es
+	logger.Logf("startBundle: %v %v - %v elements at %v", ss.ID, rb.bundleID, len(es.es), rb.watermark)
+	return true
+}
+
+// minimumPendingTimestamp returns the minimum pending timestamp from all pending elements,
+// including in progress ones.
+//
+// Assumes that the pending heap is initialized if it's not empty.
+func (ss *stageState) minPendingTimestamp() mtime.Time {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	minPending := mtime.MaxTimestamp
+	if len(ss.pending) != 0 {
+		minPending = ss.pending[0].timestamp
+	}
+	for _, es := range ss.inprogress {
+		minPending = mtime.Min(minPending, es.minTimestamp)
+	}
+	return minPending
+}
+
+func (ss *stageState) String() string {
+	pcol, up := ss.UpstreamWatermark()
+	return fmt.Sprintf("[%v] IN: %v OUT: %v UP: %q %v", ss.ID, ss.input, ss.output, pcol, up)
 }
 
 // updateWatermarks performs the following operations:
@@ -628,18 +611,36 @@ func (ss *stageState) updateWatermarks(minPending, minStateHold mtime.Time, em *
 	return refreshes
 }
 
-type set[K comparable] map[K]struct{}
-
-func (s set[K]) remove(k K) {
-	delete(s, k)
-}
-
-func (s set[K]) insert(k K) {
-	s[k] = struct{}{}
-}
-
-func (s set[K]) merge(o set[K]) {
-	for k := range o {
-		s.insert(k)
+func (ss *stageState) bundleReady(em *elementManager) (mtime.Time, bool) {
+	ss.mu.Lock()
+	defer ss.mu.Unlock()
+	// If the upstream watermark and the input watermark are the same,
+	// then we can't yet process this stage.
+	inputW := ss.input
+	upstreamPcol, upstreamW := ss.UpstreamWatermark()
+	if inputW == upstreamW {
+		logger.Logf("Bundles: stage %v has insufficient upstream watermark; requires %q %v > %v", ss.ID, upstreamPcol, inputW, upstreamW)
+		return mtime.MinTimestamp, false
+	} else if inputW > upstreamW {
+		logger.Fatalf("%v has invariance violation, input watermark greater than upstream: %v", ss.ID, inputW, upstreamW)
+		return mtime.MinTimestamp, false
 	}
+
+	// We know now that inputW < upstreamW, so we should be able to process any available inputs.
+	if len(ss.sides) > 0 {
+		logger.Logf("Bundles: stage %v has side inputs %v", ss.ID, ss.sides)
+	}
+	ready := true
+	for _, side := range ss.sides {
+		pID := em.pcolParents[side]
+		parent := em.stages[pID]
+		ow := parent.OutputWatermark()
+		logger.Logf("Bundles: stage %v parent[%v] side input %v watermark; requires %v >= %v", ss.ID, pID, side, ow, upstreamW)
+
+		if upstreamW > ow {
+			logger.Logf("Bundles: stage %v side input %v from %v insufficient watermark; requires %v >= %v", ss.ID, side, pID, ow, upstreamW)
+			ready = false
+		}
+	}
+	return upstreamW, ready
 }
